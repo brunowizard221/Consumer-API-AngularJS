f8e497478f4e42cd8b0d210053ec8091da0f82a2
Bruno barros <brunobba221@hotmail.com>
2020-08-12 16:35:42 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Projeto teste de consumo de API.
+# Back-end em Rails, localizado em https://tranquil-waters-16323.herokuapp.com/contacts.
+# Front-end em AngularJS.
+ 
---
+++ js/app/controllers.js
@@ -0,0 +1,55 @@
+var app = angular.module("meuModulo", []);
+app.controller("indexController", function ($scope, $http) {
+    var url = "https://tranquil-waters-16323.herokuapp.com/contacts";
+    $scope.editing = false;
+
+    $scope.loadContact = function () {
+        $http.get(url).success(function (response) {
+            $scope.contacts = response;
+        });
+    }
+
+    $scope.openAddContact = function () {
+        $scope.editing = false;
+        limpaform();
+        $("#modal1").openModal();
+    }
+
+    $scope.addContact = function (contact) {
+        $http.post("https://tranquil-waters-16323.herokuapp.com/contacts/", contact).then($scope.loadContact);
+        $('#modal1').closeModal();
+        limpaform();
+    }
+
+    $scope.editContact = function (contact) {
+        $scope.editing = true;
+        $scope.contact = angular.copy(contact);
+        $("#modal1").openModal();
+
+    }
+
+    $scope.saveContact = function (contact) {
+        $http.put(`https://tranquil-waters-16323.herokuapp.com/contacts/${contact.id}`, contact).then($scope.loadContact);
+        $('#modal1').closeModal();
+    }
+
+    $scope.deleteContact = function (contact, i) {
+        // for(var index in $scope.contacts){
+        //     var aux = $scope.contacts[index];
+        //     if(question === aux){
+        //          $scope.contacts.splice(index,1);
+        //     }
+        // }
+
+        $http.delete(`https://tranquil-waters-16323.herokuapp.com/contacts/${contact.id}`).then(function (response) {
+            $scope.loadContact();
+         // $scope.contacts.splice(i, 1);
+        });
+    }
+
+    var limpaform = function () {
+        $scope.contact = { name: "", email: "", phone: "" };
+    }
+    $scope.loadContact();
+
+});
